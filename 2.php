16e9738e05144cc85d6df16e022d93386288a8b9
khiughan1@mail.ru <khiughan1@mail.ru>
2020-12-31 22:13:44 +0300

    TheSecondTaskSecondObjective

--- 2.php
+++ 2.php
@@ -1,5 +1,41 @@
 
 <?php
+    function mySortForKey($a, $b)
+    {
+        try {
+           foreach($a as $key => $value)
+            {
+                foreach($value as $k => $v)
+                {
+                    if(!array_key_exists($b,$value))
+                    {
+                        throw new Exception (array_search(next($value), $value));
+                    }
+                }
+            }
+            usort($a, function ($x, $y) use ($b) {
+                if ($x[$b] > $y[$b]) {
+                    return true;
+                } else if ($x[$b] < $y[$b]) {
+                    return false;
+                } else {
+                    return 0;
+                }
+            });
+            print_r($a);
+        } catch(Exception $e)
+        {
+            echo $e->getMessage();
+        }
+    }
+
+    $arr = [['a' => 1, 'b' => 6],
+            ['a' => 5, 'b' => 4],
+            ['a' => 4, 'b' => 7]];
+
+    mySortForKey($arr, 'b');
+
+
     function  convertString($a, $b)
         {
             if(stripos($a, $b))
